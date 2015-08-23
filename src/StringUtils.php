f8354a3ea3badc5864d83a89582dde99dda694f5
xjstratedgebx <jarret@solvomedia.com>
2015-08-23 18:52:34 -0400

    Adding a utility class for strings

---
+++ src/StringUtils.php
@@ -0,0 +1,45 @@
+<?php
+namespace Stratedge\Toolbox;
+
+class StringUtils
+{
+    /**
+     * Given a string that is snake-cased, will convert the string to camelcase,
+     * optionally with the first character capitalized
+     * 
+     * @param  string  $str
+     * @param  bool $capitalize_first
+     * @return string
+     */
+    function toCamelCase($str, $capitalize_first = false)
+    {
+        $parts = explode("_", $str);
+
+        foreach($parts as $key => &$part) {
+            $part = strtolower($part);
+            if($capitalize_first == true || $key > 0) $part = ucfirst($part);
+        }
+
+        return implode(null, $parts);
+    }
+
+
+    /**
+     * Given a string that is camelcased, will convert the string to snake-case
+     * @param  string  $str
+     * @return string
+     */
+    function toSnakeCase($str)
+    {
+        preg_match_all('/[A-Z][^A-Z]+/', ucfirst($str), $matches);
+        
+        $matches = array_map(
+            function ($val) {
+                return strtolower($val);
+            },
+            $matches[0]
+        );
+
+        return implode("_", $matches);
+    }
+}
